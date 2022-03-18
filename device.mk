b665c7ae7a3293c3a3c5fe48ec9ceaa325173a3d
Tuan Anh <60842977+log1cs@users.noreply.github.com>
2022-03-18 23:47:09 +0700

    NB1: build android.hardware.radio.config@1.0 from source

--- device.mk
+++ device.mk
@@ -438,6 +438,7 @@ PRODUCT_COPY_FILES += \
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.0 \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor \
